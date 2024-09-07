577bf06f4f3385efd1f34f812163dfe2d5d3e250
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-06 20:45:13 -0700

    Add "/" before content file paths.

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -18,9 +18,9 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content="images/BrettHeadshotBW.jpg" />
+	<meta property="og:image" content="/images/BrettHeadshotBW.jpg" />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content="images/BrettHeadshotBW.jpg" />
+	<meta name="twitter:image" content="/images/BrettHeadshotBW.jpg" />
 </svelte:head>
