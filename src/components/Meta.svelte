9cc68222eca4badce7b463022b9b56f36f8dae99
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-07 14:31:48 -0700

    Change photo to Brett-headshot. Go back to fullImageURL pattern.

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -5,24 +5,23 @@
 	export let description =
 		'Brett Austin Eastman is a software engineer and musician based in San Francisco. He specializes in React, Svelte, Node, Firebase, MongoDB, and PostgreSQL.';
 	export let keywords = 'software, engineer, web development';
-	export let ogImage = '/images/BrettHeadshotBW.jpg';
+	export let ogImage = '/images/Brett-headshot.jpeg';
 
 	$: fullImageURL = new URL(ogImage, $page.url.origin).href;
 </script>
 
 <svelte:head>
 	<title>{title}</title>
+
 	<meta name="description" content={description} />
 	<meta name="keywords" content={keywords} />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="website" />
-	<!-- <meta property="og:image" content={fullImageURL} /> -->
-	<meta property="og:image" content="https://www.bretteastman.dev/images/BrettHeadshotBW.jpg" />
+	<meta property="og:image" content={fullImageURL} />
 
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<!-- <meta name="twitter:image" content={fullImageURL} /> -->
-	<meta name="twitter:image" content="https://www.bretteastman.dev/images/BrettHeadshotBW.jpg" />
+	<meta name="twitter:image" content={fullImageURL} />
 </svelte:head>
