0440f3202a4d056007dca83c7c1c61cdeb7c2a58
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-06 20:55:36 -0700

    Trying the full URL hardcoded into the content again.

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -17,9 +17,12 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content={fullImageURL} />
+	<!-- <meta property="og:image" content={fullImageURL} /> -->
+	<meta property="og:image" content="https://www.bretteastman.dev/images/BrettHeadshotBW.jpg" />
+
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={fullImageURL} />
+	<!-- <meta name="twitter:image" content={fullImageURL} /> -->
+	<meta name="twitter:image" content="https://www.bretteastman.dev/images/BrettHeadshotBW.jpg" />
 </svelte:head>
