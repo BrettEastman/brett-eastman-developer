10d7744ac901efd06a1de310134093cd5a572c66
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-07 15:11:14 -0700

    Add width and height to og image. Delete BrettHeadshotBW.

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -4,24 +4,29 @@
 	export let title = 'Brett Eastman - Full Stack Developer';
 	export let description =
 		'Brett Austin Eastman is a software engineer and musician based in San Francisco. He specializes in React, Svelte, Node, Firebase, MongoDB, and PostgreSQL.';
-	export let keywords = 'software, engineer, web development';
+	export let keywords = 'software development, web design, SEO';
 	export let ogImage = '/images/Brett-headshot.jpeg';
 
-	$: fullImageURL = new URL(ogImage, $page.url.origin).href;
+	$: fullUrl = $page.url.href;
+	$: fullImageUrl = new URL(ogImage, $page.url.origin).href;
 </script>
 
 <svelte:head>
 	<title>{title}</title>
-
 	<meta name="description" content={description} />
 	<meta name="keywords" content={keywords} />
+
+	<meta property="og:url" content={fullUrl} />
+	<meta property="og:type" content="website" />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:type" content="website" />
-	<meta property="og:image" content={fullImageURL} />
+	<meta property="og:image" content={fullImageUrl} />
+	<meta property="og:image:width" content={'1200'} />
+	<meta property="og:image:height" content={'630'} />
+	<meta property="og:image:type" content="image/jpeg" />
 
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={fullImageURL} />
+	<meta name="twitter:image" content={fullImageUrl} />
 </svelte:head>
