8f6a7bd11356d03729e82abd2bea8b04c61e4f18
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-07 22:25:27 -0700

    Change og:image:height to 1200.

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -22,7 +22,7 @@
 	<meta property="og:description" content={description} />
 	<meta property="og:image" content={fullImageUrl} />
 	<meta property="og:image:width" content={'1200'} />
-	<meta property="og:image:height" content={'630'} />
+	<meta property="og:image:height" content={'1200'} />
 	<meta property="og:image:type" content="image/jpeg" />
 
 	<meta name="twitter:card" content="summary_large_image" />
