b8a8d4bbf6df49585e10a83fc6adc575abbf7e05
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-06-20 18:37:59 -0700

    fix: Upgrade to Tailwind 4 syntax in app.css, adding @theme to hold all the CSS color variables, removing them from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			colors: {
-				primary10: '#0D0E0B',
-				primary20: '#2B2B2B',
-				primary30: '#696969',
-				primary40: '#848484',
-				primary50: '#A0A0A0',
-				primary60: '#B7B8B6',
-				primary70: '#C9CAC8',
-				primary80: '#EEEEEE',
-				primary90: '#FCFCFC',
-				primary100: '#FFFFFF',
-				pear: '#D6BB7D',
-				peach: '#E59D5E',
-				peachGrey: '#DBA784',
-				greyPeach: '#D3B4A0',
-				blueRedGrey: '#b9b1bc',
-				blueGrey: '#B0AFC4',
-				warning: '#B3261E',
-				warningHover: '#A31F19'
-			}
-		}
-	},
+	theme: {},
 	plugins: []
 };
